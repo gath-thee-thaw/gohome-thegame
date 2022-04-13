d874be12dfe284319679368215ad87ca4bea2f33
Dreadi01 <inukasenanayake@gmail.com>
2022-04-13 13:07:43 +0530

    daef

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,28 +7,45 @@ export var speed := 500
 # See how we use it below to update the character's look direction in the game.
 var _sprites := {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
 var _velocity := Vector2.ZERO
+var flip
+var direction
 
-
+var bullet = load("res://scenes/bullet.tscn")
 
 
 func _physics_process(_delta: float) -> void:
 	# Once again, we call `Input.get_action_strength()` to support analog movement.
-	var direction := Vector2(
-		# This first line calculates the X direction, the vector's first component.
+	direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		# And here, we calculate the Y direction. Note that the Y-axis points 
-		# DOWN in games.
-		# That is to say, a Y value of `1.0` points downward.
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	# When aiming the joystick diagonally, the direction vector can have a length 
-	# greater than 1.0, making the character move faster than our maximum expected
-	# speed. When that happens, we limit the vector's length to ensure the player 
-	# can't go beyond the maximum speed.
+	if(Input.get_action_strength("right")):
+		if(flip):
+			apply_scale(Vector2(-1,1))
+			flip = false
+	if(Input.get_action_strength("left")):
+		if(!flip):
+			apply_scale(Vector2(-1,1))
+			flip = true
+	
+	$rifle.look_at(get_global_mouse_position())
+	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	move_and_slide(speed * direction)
+	
+	if Input.is_action_just_pressed("shoot"):
+		Shoot()
 
 
+func Shoot():
+	
+	var bul = bullet.instance()
+	bul.position = $rifle/spawnPoint.global_position
+	bul.rotation_degrees = $rifle.rotation_degrees
+	bul.playerDirection = direction
+	bul.playerSpeed = speed
+	get_tree().root.add_child(bul)
+	
 
 
