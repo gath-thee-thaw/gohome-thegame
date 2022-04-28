bae529cefdbc163043fbb6624195c0ff99e7b5de
Dreadi01 <inukasenanayake@gmail.com>
2022-04-28 18:09:24 +0530

    maidage animation dahanko

--- Scripts/bulletSMG.gd
+++ Scripts/bulletSMG.gd
@@ -6,10 +6,14 @@ export (float, 0, 0.1) var recoil = 0.08
 var dir
 var pos
 
+var curent_pos
+var old_pos
+
 func _ready():
 	$AnimatedSprite.play("default")
 	var val = rand_range(-recoil, recoil)
 	dir = Vector2(position.x + 1, position.y - val) - position
+	old_pos = global_position
 
 func _process(delta):
 	#position += playerDirection * playerSpeed + transform.x * speed * delta //playerDirection * playerSpeed + 
@@ -18,12 +22,26 @@ func _process(delta):
 		position += dir * speed * delta
 	else:
 		position += -dir * speed * delta
-
-func _on_bullet2_body_entered(_body):
-	queue_free()	
+		
 	
-func _on_bullet2_area_entered(area):
-	if area.is_in_group("enemy"):
-		Global.frame_freeze(0.05, 0.4)
-		area.damage(1, position)
+	curent_pos = global_position
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(curent_pos, old_pos, [], 2147483647, true, true)
+	
+	
+	if result.size() != 0:
+		if result.collider.is_in_group("enemy"):
+		   Global.frame_freeze(0.05, 0.4)
+		   result.collider.damage(1, position)
+		if result.collider != self:
+			queue_free()
+
+	
+	old_pos = curent_pos
+	
+
+
+
+
+func _on_Timer_timeout():
 	queue_free()
