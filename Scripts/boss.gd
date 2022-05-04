a4137d33a1fa5ef96b1b87371d8d22ad1c86403a
Dreadi01 <inukasenanayake@gmail.com>
2022-05-04 09:37:51 +0530

    boss

---
+++ Scripts/boss.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+var enemychase = load("res://Scenes/Enemies/EnemyChase.tscn")
+var enemyGun = load("res://Scenes/Enemies/EnemyGun.tscn")
+var maxteleportRadius = 500
+var minteleportRadius = -500
+
+
+
+
+func _on_SpawnTimer_timeout():
+	_spawn_enemies()
+	
+	
+	
+
+func _spawn_enemies():
+	for n in $SpawnPoints.get_child_count():
+		var enemy = enemychase.instance()
+		enemy.position = $SpawnPoints.get_child(n).global_position
+		get_tree().root.add_child(enemy)
+
+
+func _on_TeleportTimer_timeout():
+	var xRand = rand_range(minteleportRadius, maxteleportRadius)
+	var yRand = rand_range(minteleportRadius, maxteleportRadius)
+	global_position = global_position + Vector2(xRand, yRand)
--- Scripts/joi.gd
+++ Scripts/joi.gd
@@ -1,6 +1,23 @@
 extends TouchScreenButton
 
+onready var big = get_parent().get_parent()
+onready var small = get_parent()
+
+var touched = false
+
 func _input(event):
 	if event is InputEventScreenDrag or event is InputEventScreenTouch:
-		global_position = event.position
+		if not touched:
+			touched = true
+		else:
+			touched = false
+			small.position = Vector2(0, 0)
+		
+		
+		
+func _process(delta):
+	if touched:
+		small.global_position = get_global_mouse_position()
+		small.global_position = big.global_position + (small.global_position - big.global_position).clamped(80)
 
+	
