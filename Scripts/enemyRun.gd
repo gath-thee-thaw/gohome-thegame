08f6ab31896af552b46237290472804ebece89ae
gath-thee-thaw <52409659+gath-thee-thaw@users.noreply.github.com>
2022-05-06 06:09:21 +0530

    enemychase knockback

--- Scripts/enemyRun.gd
+++ Scripts/enemyRun.gd
@@ -11,6 +11,10 @@ var flip
 var health = 5
 var rng = RandomNumberGenerator.new()
 
+export var knockback = 50
+var isknockback = false
+var current_xpos
+
 func _process(delta):
 
 	var direction = Vector2(
@@ -25,21 +29,32 @@ func _process(delta):
 		speed = 0
 	else:
 		speed = kSpeed
-	   
-	if(player.position.x > position.x):
+		
+	#flip
+	if(player.global_position.x > global_position.x):
+		knockback = -abs(knockback)
 		if(flip):
 			apply_scale(Vector2(-1,1))
 			flip = false
-	if(player.position.x < position.x):
+
+	if(player.global_position.x < global_position.x):
+		knockback = abs(knockback)
 		if(!flip):
 			apply_scale(Vector2(-1,1))
 			flip = true
 
+	if isknockback:
+		global_position.x = lerp(global_position.x, current_xpos + knockback, 0.6)
+		isknockback = false
+
 func damage(var num, pos):
+	$ImpactAnimationPlayer.stop(true)
+	$ImpactAnimationPlayer.play("Hit")
 	var bp = Global.blood_splash.instance()
 	add_child(bp)
 	bp.global_position = pos
-	
+	isknockback = true
+	current_xpos = global_position.x
 	health -= num
 	if(health <= 0):
 		Global.enemies -= 1
